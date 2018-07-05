aa84d8be270bf1432361012dacd155ac90c2b8c2
Hillary Lavin <hilavin@gmail.com>
2018-07-05 15:15:38 -0400

    Comment out requirements for testing

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+// const path = require("path");
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
 // const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
 // Add routes, both API and view
 // app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
